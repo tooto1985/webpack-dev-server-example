c57c12206bf04f01c5b961c9f50b10a6c815ad76
Tommy <tooto1985@gmail.com>
2018-03-23 17:17:29 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+// const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   entry: ['./src/index.js', './src/index.scss'],
 
@@ -7,7 +9,14 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  cache: false,
   plugins: [
+    // new webpack.HotModuleReplacementPlugin(),
+    // new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html',
@@ -21,7 +30,7 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
       }
     ]
   }
